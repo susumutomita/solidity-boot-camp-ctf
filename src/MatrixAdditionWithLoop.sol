7c029eb6fcb7a22138116254d96c6692a90d758d
susumu tomita <oyster880@gmail.com>
2024-03-17 07:29:20 +0900

    update

--- src/MatrixAdditionWithLoop.sol
+++ src/MatrixAdditionWithLoop.sol
@@ -13,10 +13,13 @@ contract MatrixAdditionWithLoop is IsolutionLoop {
         uint256[2][2] calldata x,
         uint256[2][2] calldata y
     ) external pure override returns (uint256[2][2] memory) {
-        uint256[2][2] memory result = [
-            [x[0][0] + y[0][0], x[0][1] + y[0][1]],
-            [x[1][0] + y[1][0], x[1][1] + y[1][1]]
-        ];
+        uint256[2][2] memory result;
+
+        for (uint i = 0; i < 2; i++) {
+            for (uint j = 0; j < 2; j++) {
+                result[i][j] = x[i][j] + y[i][j];
+            }
+        }
 
         return result;
     }
