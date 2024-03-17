4e9e5567a50fd0413a760d4d69801f29afd10c34
susumu tomita <oyster880@gmail.com>
2024-03-17 21:35:47 +0900

    update

--- src/Level1MatrixAddition.sol
+++ src/Level1MatrixAddition.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-interface Isolution {
-    function solution(
-        uint256[2][3] calldata x,
-        uint256[2][3] calldata y
-    ) external pure returns (uint256[2][3] memory);
-}
+// interface Isolution {
+//     function solution(
+//         uint256[2][3] calldata x,
+//         uint256[2][3] calldata y
+//     ) external pure returns (uint256[2][3] memory);
+// }
 
 contract Level1MatrixAddition {
     function solution(
