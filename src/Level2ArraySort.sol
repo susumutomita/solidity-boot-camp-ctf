ee396357a3fcee638810dd7d77ceecd614aca98f
susumu tomita <oyster880@gmail.com>
2024-03-17 20:07:29 +0900

    update

--- src/Level2ArraySort.sol
+++ src/Level2ArraySort.sol
@@ -6,24 +6,35 @@ pragma solidity ^0.8.24;
     function solution(uint256[10] calldata unsortedArray) external returns (uint256[10] memory sortedArray);
   }
 */
+
 contract Level2ArraySort {
     function solution(
         uint256[10] calldata unsortedArray
     ) external pure returns (uint256[10] memory) {
-        uint256[10] memory sortedArray = unsortedArray;
-        bool swapped;
-        do {
-            swapped = false;
-            for (uint i = 0; i < sortedArray.length - 1; i++) {
-                if (sortedArray[i] > sortedArray[i + 1]) {
-                    (sortedArray[i], sortedArray[i + 1]) = (
-                        sortedArray[i + 1],
-                        sortedArray[i]
-                    );
-                    swapped = true;
-                }
+        uint256[10] memory arr = unsortedArray;
+        quickSort(arr, 0, int(arr.length - 1));
+        return arr;
+    }
+
+    function quickSort(
+        uint256[10] memory arr,
+        int left,
+        int right
+    ) internal pure {
+        int i = left;
+        int j = right;
+        if (i >= j) return;
+        uint pivot = arr[uint(left + (right - left) / 2)];
+        while (i <= j) {
+            while (arr[uint(i)] < pivot) i++;
+            while (pivot < arr[uint(j)]) j--;
+            if (i <= j) {
+                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                i++;
+                j--;
             }
-        } while (swapped);
-        return sortedArray;
+        }
+        if (left < j) quickSort(arr, left, j);
+        if (i < right) quickSort(arr, i, right);
     }
 }
