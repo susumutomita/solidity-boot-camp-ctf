f1c771ac2b0f71851fac4fdd7e2d920ee518576d
susumu tomita <oyster880@gmail.com>
2024-03-21 10:31:19 +0900

    update

--- src/Level6VerifyingSignatures.sol
+++ src/Level6VerifyingSignatures.sol
@@ -12,13 +12,16 @@ pragma solidity ^0.8.24;
 // }
 
 contract Level6VerifyingSignatures {
-    function solution(bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) external pure returns (address signer) {
-        // Ethereumで署名メッセージを検証する際のプレフィックス
-        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        // プレフィックス付きのメッセージハッシュを計算
-        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked(prefix, messageHash));
+    function solution(
+        bytes32 messageHash,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external pure returns (address signer) {
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+        );
 
-        // ecrecover関数を使用して署名者のアドレスを復元
         signer = ecrecover(ethSignedMessageHash, v, r, s);
 
         return signer;
---
+++ src/Level6VerifyingSignaturesTemplate.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+/*
+interface Isolution6 {
+    function solution(address owner, bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) external pure returns (bool isSignedByOwner);
+}
+*/
+
+contract Level6VerifyingSignaturesFromTemplate {
+    function solution(
+        address owner,
+        bytes32 messageHash,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external pure returns (bool isSignedByOwner) {
+        return isSignedByOwner = owner == ecrecover(messageHash, v, r, s);
+    }
+}
